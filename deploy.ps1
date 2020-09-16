a992b129a151ae5c11c35eaab5fa89dd405a4d88
Shawn Gibbs <shawngib@microsoft.com>
2020-09-16 12:29:55 -0400

    script

---
+++ deploy.ps1
@@ -0,0 +1,3 @@
+$url = "https://raw.githubusercontent.com/shawngib/project-stig/master/azuredeploy.json"
+
+New-AzSubscriptionDeployment -Name demoSubDeployment -Location usgovarizona -name TestSubdeploy -location  -TemplateUri ($url)
