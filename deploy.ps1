b2aba5fb4065087559a5d91c4b8e72b525073b28
Shawn Gibbs <shawngib@microsoft.com>
2020-09-16 12:48:21 -0400

    fixed template workspace

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,8 @@
 $url = "https://raw.githubusercontent.com/shawngib/project-stig/master/azuredeploy.json"
 
-New-AzSubscriptionDeployment -Name "demoSubDeployment" -Location "usgovarizona" -rgName "TestSubdeploy" -rgLocation "usgovarizona" -TemplateUri ($url)
+New-AzSubscriptionDeployment `
+  -Name demoSubDeployment `
+  -Location usgovvirginia `
+  -TemplateUri $url `
+  -rgName TestSubdeploy `
+  -rgLocation usgovvirginia
