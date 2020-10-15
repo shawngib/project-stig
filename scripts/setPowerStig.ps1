3d02baf6064a3924c8d9ea38b4e5e99a1e5475dc
Shawn Gibbs <shawngib@microsoft.com>
2020-10-15 13:49:33 -0400

    File upload errors after STIG, moved before / task

--- scripts/setPowerStig.ps1
+++ scripts/setPowerStig.ps1
@@ -113,3 +113,22 @@ $json = $computerJsonPayload | ConvertTo-Json
 $json 4>&1 >> c:\imagebuilder\verbose.txt
 Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
 
+$STName = "PowerSTIG Audit Task"
+$STPath = "\PowerSTIG"
+$scheduleObject = New-Object -ComObject schedule.service
+$scheduleObject.connect()
+$taskRootFolder = $scheduleObject.GetFolder("\")
+$taskRootFolder.CreateFolder($STPath)
+
+$STDescription = "A task that will audit PowerSTIG DSC settings and report to Log Analytics."
+$STAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\auditStig.ps1"
+$STTrigger = New-ScheduledTaskTrigger -Daily -At 12am
+$STSettings = New-ScheduledTaskSettingsSet
+$STUserName = "NT AUTHORITY\SYSTEM"
+Register-ScheduledTask -TaskPath $STPath -TaskName $STName -Description $STDescription -Action $STAction -Trigger $STTrigger -RunLevel Highest -Settings $STSettings -User $STUserName 
+Start-Sleep -Seconds 3
+
+$STModify = Get-ScheduledTask -TaskName $STName
+$STModify.Triggers.repetition.Duration = 'P1D'
+$STModify.Triggers.repetition.Interval = 'PT20M'
+$STModify | Set-ScheduledTask
