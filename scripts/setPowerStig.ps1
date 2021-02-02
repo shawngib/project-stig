4c09ee94cc3f81db135933127d3c198dcd747ce1
Shawn Gibbs <shawngib@microsoft.com>
2021-02-02 10:18:20 -0500

    Update version and fix bugs in setPowerSTIG script

--- scripts/setPowerStig.ps1
+++ scripts/setPowerStig.ps1
@@ -1,45 +1,26 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-$path = "c:\imageBuilder"
-$logFile = "$path\setupLog.txt"
-function LogMessage
-{
-    param([string]$message)
-    
-    ((Get-Date).ToString() + " - " + $message) >> $logFile;
-}
-
-# Using PowerShell New-Item asks permission, these command do not.
-mkdir -Path $path
-cd -Path $path
-
-$computerInfo = Get-ComputerInfo
-$powerStigVersion = $env:POWERSTIG_VER
-$domainRole = $env:STIG_OSROLE
-$windowsInstallationType = $computerInfo.WindowsInstallationType
 $model = $env:STIG_OSVER
 $stigVersion = $env:STIG_VER
 
-LogMessage -message "Starting setPowerStig.ps1"
+LogMessage -message "**** Setting TLS"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ if($windowsInstallationType -eq 'Client') 
+ {
+    LogMessage -message "**** Setting execution policy for clinet type"
+    Set-ExecutionPolicy Unrestricted -Force 2>>$logFile # Windows 10 only
+ }
 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+LogMessage -message "**** Installing NuGet"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force  2>>$logFile
 # Install/Import PowerStig
 LogMessage -message "**** Installing PowerStig Module"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module PowerStig -RequiredVersion $powerStigVersion -Force
-#Install-Module ProcessMitigations -Force -SkipPublisherCheck
+Install-Module PowerStig -RequiredVersion $powerStigVersion 2>>$logFile -Force
+
+LogMessage -message "**** Installing additional PowerStig Module requirements"
 (Get-Module PowerStig -ListAvailable).RequiredModules | % {
-    $PSItem | Install-Module -Force
- } 
- if((Get-ComputerInfo).WindowsInstallationType -eq 'Client') 
- {
-    LogMessage -message "**** Setting execution policy"
-    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force # Windows 10 only
+    $PSItem | Install-Module -Force 2>>$logFile
  }
 LogMessage -message "**** Importing PowerStig Module"
-Import-Module PowerStig -Force
+Import-Module PowerStig -Force 2>>$logFile
 
 # Enable WSMan / WinRm
 LogMessage -message "**** Installing WSMAN, setting MaxEvelopeSize and disabling PSremoting"
@@ -47,10 +28,10 @@ Set-WSManQuickConfig -Force
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizekb -Value 8192 # PowerSTIG DSC requires larger envelope size. 
 #Disable-PSRemoting # PowerShell remoting required so disable it.
 
-LogMessage -message "**** Running DscConfiguration Test"
+LogMessage -message "**** Running DscConfiguration Test and logging to verbose.txt"
 $null = Start-DscConfiguration -Path "c:\" -Force -Wait -Verbose 4>&1 >> c:\imagebuilder\verbose.txt
 
-LogMessage -message "**** Setting up logging to LA Workspace "
+LogMessage -message "**** Setting up logging to LA Workspace sender"
 $TimeStampField = (Get-Date).ToString()
 
 Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
