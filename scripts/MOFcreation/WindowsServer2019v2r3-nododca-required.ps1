debf5e473a1139cb1a34dc1db15328a60ee56b99
Ron <tikicoder@users.noreply.github.com>
2022-01-11 12:21:59 -0700

    Create WindowsServer2019v2r3-nododca-required.ps1
    suggesting new file based on updated powerstig

---
+++ scripts/MOFcreation/WindowsServer2019v2r3-nododca-required.ps1
@@ -0,0 +1,41 @@
+configuration WindowsServer2019v2r1
+{
+    param()
+    Import-DscResource -ModuleName PowerSTIG -ModuleVersion 4.11.0
+    Node localhost
+    {
+        WindowsServer BaseLine
+        {
+            OsVersion   = '2019'
+            OsRole      = 'MS'
+            SkipRule    = 'V-205850', 'V-214936', 'V-205810', 'V-205737.b','V-205648.a', 'V-205648.b', 'V-205648.c', 'V-205648.d', 'V-205649.a', 'V-205649.b', 'V-205650.d', 'V-205650.b'
+            StigVersion = '2.3'
+            Exception   = @{
+                'V-205715' = @{
+                    ValueData = '1' # Required for using Azure Image Builder access to creation
+                }
+                'V-205733' = @{
+                    Identity = 'Guests' 
+                }
+                'V-205672' = @{
+                    Identity = 'Guests'
+                }
+                'V-205673' = @{
+                    Identity = 'Guests'
+                }
+                'V-205675' = @{
+                    Identity = 'Guests'
+                }
+            }
+        }
+        InternetExplorer InternetExplorerSettings
+        {
+            StigVersion = '2.3'
+        }
+        Chrome ChromeSettings
+        {
+            StigVersion = '2.3'
+        }
+    }
+}
+WindowsServer2019v2r1 -Output c:\imagebuilder
