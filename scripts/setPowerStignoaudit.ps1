6904191822f25d7514c37c2263ac709ee2c544c2
Shawn Gibbs <shawngib@microsoft.com>
2022-08-11 11:41:13 -0400

    seperate small and large template created

--- scripts/setPowerStignoaudit.ps1
+++ scripts/setPowerStignoaudit.ps1
@@ -57,4 +57,39 @@ LogMessage -message "**** Running DscConfiguration and logging to verbose.txt"
 $null = Start-DscConfiguration -Path "c:\" -Force -Wait -Verbose 4>&1 >> c:\imagebuilder\verbose.txt
 
 LogMessage -message "**** Setting up logging to LA Workspace sender"
-$TimeStampField = (Get-Date).ToString()
+
+LogMessage -message "**** Starting download and install of EDGE and SCAP tooling."
+
+$localDownloadPath = join-path $env:temp download
+if(!(Test-Path -Path $localDownloadPath)) 
+{ 
+     md -Path $localDownloadPath -ErrorAction SilentlyContinue | Out-Null
+}
+
+# Download and install Edge
+
+$edgeDownloadUrl = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/dba89b53-9614-4a7c-b1f3-85e5dd724930/MicrosoftEdgeEnterpriseX64.msi'
+
+$edgeFilePath = join-path $localDownloadPath MicrosoftEdgeEnterpriseX64.msi
+
+Invoke-RestMethod -Uri $edgeDownloadUrl -OutFile $edgeFilePath 
+
+Start-Process -FilePath $edgeFilePath -ArgumentList "/quiet"
+
+# Download, unzip and install SCAP
+
+$windowsSCAPUrl = "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/scc-5.5_Windows_bundle.zip"
+
+$scapFilePath = join-path $localDownloadPath scc-5.5_Windows_bundle.zip
+
+Invoke-RestMethod -Uri $windowsSCAPUrl -OutFile $scapFilePath 
+
+# Silently unzip install SCAP
+
+Expand-Archive -LiteralPath $scapFilePath -DestinationPath $localDownloadPath
+
+$sccPath = Join-Path $localDownloadPath scc-5.5_Windows\SCC_5.5_Windows_Setup.exe
+
+Start-Process -FilePath $sccPath -ArgumentList "/VERYSILENT /DIR=`"C:\SCC`" /TYPE=custom /COMPONENTS=`"Content\NIST_USGCB_SCAP_Content,Content\DISA_STIG_SCAP_Content,Other\SCC_Service`""
+
+Remove-Item -Path $localDownloadPath -Confirm:$false -Force -Recurse
